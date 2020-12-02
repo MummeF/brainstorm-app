e6cdd75748c57b773a9367f56e43747a1d393fc0
Gimliboy <kiesslingj.paul@gmail.com>
2020-12-02 18:00:36 +0100

    basic layout: Create Phase; Recyclerview with multiple ViewHolder; Items can be added;

--- app/src/main/java/com/dhbw/brainstorm/RoomActivity.kt
+++ app/src/main/java/com/dhbw/brainstorm/RoomActivity.kt
@@ -6,8 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class RoomActivity : AppCompatActivity() {
@@ -20,9 +20,10 @@ class RoomActivity : AppCompatActivity() {
         setContentView(R.layout.activity_room)
         findViewById<RecyclerView>(R.id.contributionList).adapter = adapter_value
 
-        findViewById<Button>(R.id.addContribution).setOnClickListener{
+        findViewById<View>(R.id.addContribution).setOnClickListener{
             addContribution()
         }
+
     }
 
 
@@ -30,35 +31,86 @@ class RoomActivity : AppCompatActivity() {
     private fun generateDummyList(): ArrayList<ContributionContent>{
         val tmpContributionList = ArrayList<ContributionContent>()
         for (i in 0 until 5){
-            tmpContributionList.add(ContributionContent("Beitrag " + i.toString()))
+            tmpContributionList.add(ContributionContent("Beitrag " + i.toString(), 1))
         }
         return tmpContributionList
     }
 
     // adding new contribution to the recyclerlist
     private fun addContribution() {
-        val newContribution = ContributionContent("the new item")
+        val newContribution = ContributionContent("the new item", 0)
+        item_list.add(item_list.count(), newContribution)
+        adapter_value.notifyItemInserted(item_list.count())
+    }
+
+    fun confirmNewContribution(view: View){
+        // get the text from the EditText
+        val editTextView = findViewById<EditText>(R.id.editTextNewContribution)
+        val newContent = editTextView.text.toString();
+
+        // remove the item with the EditText from the list
+        item_list.removeAt(item_list.count() - 1)
+        adapter_value.notifyItemRemoved(item_list.count() -1 )
+
+        // add new Contribution with the text from the EditText
+        val newContribution = ContributionContent(newContent, 1)
         item_list.add(item_list.count(), newContribution)
         adapter_value.notifyItemInserted(item_list.count())
     }
 }
 
-data class ContributionContent(val textContent: String)
+data class ContributionContent(val textContent: String, val viewType: Int)
 
-class MyAdapter(private val data_list: List<ContributionContent>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>(){
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_contribution_card, parent, false)
-        return MyViewHolder(itemView)
+class MyAdapter(private val data_list: List<ContributionContent>) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    // different types of layout for the single items
+    private final val ITEM_TYPE_ADD = 0; // adding new contribution -> EditText
+    private final val ITEM_TYPE_CONTRIBUTION = 1; // normal contribution with content -> TextView
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if(viewType == 0)
+        {
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_new_contribution, parent, false)
+            return AddContributionViewHolder(itemView)
+        }
+        else
+        {
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_contribution_card, parent, false)
+            return ContributionViewHolder(itemView)
+        }
+
     }
 
     override fun getItemCount() = data_list.count()
 
-    override fun onBindViewHolder(holder: MyAdapter.MyViewHolder, position: Int) {
+    override fun getItemViewType(position: Int): Int {
+        if(data_list[position].viewType == 0)
+        {
+            return ITEM_TYPE_ADD
+        }
+        else
+        {
+            return ITEM_TYPE_CONTRIBUTION
+        }
+    }
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val currentItem = data_list[position]
-        holder.textViewContent.text = currentItem.textContent
+        val currentViewType = getItemViewType(position)
+        when(currentViewType){
+            0 -> {
+                val addContrViewHolder: AddContributionViewHolder = holder as AddContributionViewHolder;
+            }
+            1 ->{
+                val contrViewHolder: ContributionViewHolder = holder as ContributionViewHolder;
+                contrViewHolder.textViewContent.text = currentItem.textContent;
+            }
+        }
     }
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ContributionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val textViewContent: TextView = itemView.findViewById(R.id.contributionTextView)
+    }
+    class AddContributionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val editTextView: EditText = itemView.findViewById(R.id.editTextNewContribution)
     }
 }
